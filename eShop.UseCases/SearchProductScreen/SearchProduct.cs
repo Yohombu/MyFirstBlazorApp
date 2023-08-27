68581a986b99a381e780b38613c2c793c05a3753
Yohombu Abeysinghe <yohombuda@gmail.com>
2023-08-27 22:54:09 +0530

    Add project files.

---
+++ eShop.UseCases/SearchProductScreen/SearchProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eShop.CoreBusiness.Models;
+
+namespace eShop.UseCases.SearchProductScreen
+{
+    internal class SearchProduct
+    {
+        public IEnumerable<Product> Execute(string filter)
+        {
+            return null;
+        }
+    }
+}
---
+++ eShop.UseCases/SearchProductScreen/ViewProduct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eShop.CoreBusiness.Models;
+
+namespace eShop.UseCases.SearchProductScreen
+{
+    public Product Execute(string filter)
+    {
+        return null;
+    }
+}
